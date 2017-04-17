f65208329dd84b9e8008cc90e30ffa9c95130794
rwfazul <rhauaita@hotmail.com>
2017-04-17 15:52:22 -0300

    adicao bash

---
+++ t1-SO/script-media/arvore.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+START=1
+END=30
+
+ShowTime () {
+	time_finish=0
+	file=arvoreProcessos
+
+	for (( c = $START; c<=$END; c++ )) 
+	do
+		./$file $1 >> out.txt
+		tempo=`sed -n '$p' out.txt`
+		time_finish=`echo $tempo + $time_finish | bc`
+	done
+
+	echo -e "\n"
+
+	echo "total exe: " $END
+	echo "total time: " $time_finish
+
+	media=$(echo $time_finish/$END | bc -l)
+	echo "media: " $media
+	echo -e "\n"
+}
+
+gcc -o arvoreProcessos arvoreProcessos.c
+ShowTime $1
+
